54e3543a9d0eb251765ffbe8802ade0241b8512b
ByteWise <157060515+realByteWise@users.noreply.github.com>
2024-12-21 13:06:45 +0400

    Updated to work with settings.py

--- main.py
+++ main.py
@@ -4,26 +4,33 @@ from view_map import view_map
 from buy_ticket import buy_ticket
 from heatmaps import heatmaps
 from race_winner import show_podium
+from settings import show_settings
 
-show_main_menu_buttons = True
+
 pygame.init()
-pygame.mixer.init()
-pygame.mixer.music.load('assets/gas.mp3')
-pygame.mixer.music.play(-1)
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("FormulaWise")
 icon_image = pygame.image.load("assets/fw.png")
 pygame.display.set_icon(icon_image)
-bg_image = pygame.image.load(bg_image_path)
+current_bg_image_path = "assets/bgs/main.jpg"
+bg_image = pygame.image.load(current_bg_image_path)
 bg_image = pygame.transform.scale(bg_image, (WIDTH, HEIGHT))
 logo_image = pygame.image.load(logo_image_path)
 logo_image = pygame.transform.scale(logo_image, (logo_width, logo_height))
+settings_button_image = pygame.image.load("assets/settings.png")
+settings_button_rect = settings_button_image.get_rect(center=(260, 900))  
+settings_button_image = pygame.transform.scale(settings_button_image, (50, 50))
 font = pygame.font.Font(pygame.font.match_font('Palatino'), 24)
 input_width, input_height = 300, 40
+pygame.init()
+pygame.mixer.init()
+pygame.mixer.music.load('assets/music/gas.mp3')
+pygame.mixer.music.play(-1)
 username_box = pygame.Rect(WIDTH // 2 - input_width // 2, HEIGHT // 2 - 100, input_width, input_height)
 password_box = pygame.Rect(WIDTH // 2 - input_width // 2, HEIGHT // 2 - 30, input_width, input_height)
 login_button = pygame.Rect(WIDTH // 2 - 75, HEIGHT // 2 + 60, 150, 50)
 signup_button = pygame.Rect(WIDTH // 2 - 75, HEIGHT // 2 + 130, 150, 50)
+
 username_text = ""
 password_text = ""
 active_box = None
@@ -31,6 +38,7 @@ error_message = ""
 is_signing_up = False
 credentials = load_credentials()
 is_logged_in = False
+
 button_width, button_height = 150, 50
 buttons = [
     pygame.Rect(WIDTH // 2 - button_width // 2, HEIGHT // 2 - 130, button_width, button_height),
@@ -39,8 +47,8 @@ buttons = [
     pygame.Rect(WIDTH // 2 - button_width // 2, HEIGHT // 2 + 80, button_width, button_height),
     pygame.Rect(WIDTH // 2 - button_width // 2, HEIGHT // 2 + 150, button_width, button_height),
 ]
-button_colors = [GRAY, GRAY, GRAY,GRAY, RED]
-button_labels = ["VIEW MAPS", "BUY TICKETS", "HEATMAPS", "POSITIONS",'SIGN OUT']
+button_colors = [GRAY, GRAY, GRAY, GRAY, RED]
+button_labels = ["VIEW MAPS", "BUY TICKETS", "HEATMAPS", "RESULTS", 'SIGN OUT']
 
 running = True
 while running:
@@ -85,24 +93,26 @@ while running:
                         password_text = password_text[:-1]
                     else:
                         password_text += event.unicode
-
         else:  
             if event.type == pygame.MOUSEBUTTONDOWN:
                 for i, button in enumerate(buttons):
                     if button.collidepoint(event.pos):
                         if i == 0:
-                            view_map(screen)
-                            screen.blit(bg_image, (0, 0))
+                            view_map(screen,current_bg_image_path)
                         elif i == 1:
-                            buy_ticket(screen)
+                            buy_ticket(screen,current_bg_image_path)
                         elif i == 2:
-                            screen.blit(bg_image, (0, 0))
-                            heatmaps(screen) 
+                            heatmaps(screen,current_bg_image_path)
                         elif i == 3:
-                            screen.blit(bg_image, (0, 0))
-                            show_podium(screen)
+                            show_podium(screen,current_bg_image_path)
                         elif i == 4:
-                            is_logged_in=False
+                            is_logged_in = False  
+
+                if settings_button_rect.collidepoint(event.pos):
+                    current_bg_image_path = show_settings(screen,current_bg_image_path)  
+                    bg_image = pygame.image.load(current_bg_image_path).convert()
+                    bg_image = pygame.transform.scale(bg_image, (WIDTH, HEIGHT)) 
+
     screen.blit(bg_image, (0, 0))
     if not is_logged_in:
         screen.blit(logo_image, (WIDTH // 2 - 250, HEIGHT // 2 - 250))
@@ -115,8 +125,10 @@ while running:
         draw_text(screen, "Password", 24, WHITE, password_box.centerx, password_box.centery - 35, center=True)
         draw_text(screen, "Login", 24, WHITE, login_button.centerx, login_button.centery, center=True)
         draw_text(screen, "Sign Up", 24, WHITE, signup_button.centerx, signup_button.centery, center=True)
+
         if error_message:
             draw_text(screen, error_message, 20, RED, WIDTH // 2, HEIGHT // 2 + 200, center=True)
+
         draw_text(screen, username_text, 24, BLACK, username_box.centerx, username_box.centery, center=True)
         draw_text(screen, password_text, 24, BLACK, password_box.centerx, password_box.centery, center=True)
 
@@ -124,7 +136,7 @@ while running:
         for i, button in enumerate(buttons):
             pygame.draw.rect(screen, button_colors[i], button)
             draw_text(screen, button_labels[i], 24, WHITE, button.centerx, button.centery, center=True)
-
+        screen.blit(settings_button_image, settings_button_rect.topleft)
     pygame.display.flip()
 
 pygame.quit()
