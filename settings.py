838d7207c5000434d8771bd2c95cf4bee944cdac
ByteWise <157060515+realByteWise@users.noreply.github.com>
2024-12-29 09:15:36 +0400

    Remove redundant draw_text function

--- settings.py
+++ settings.py
@@ -1,5 +1,4 @@
 import sys
-import pygame
 from resources import *
 
 
@@ -8,12 +7,6 @@ music_on, current_theme_index, volume, login_date = load_preferences()
 def play_current_track():
     pygame.mixer.music.load(themes[current_theme_index][0])
     pygame.mixer.music.play(-1)
-
-def draw_text(screen, text, size, color, x, y, center=False):
-    font = pygame.font.Font(pygame.font.match_font('Palatino'), size)
-    text_surface = font.render(text, True, color)
-    text_rect = text_surface.get_rect(center=(x, y)) if center else text_surface.get_rect(topleft=(x, y))
-    screen.blit(text_surface, text_rect)
 
 def show_settings(screen, current_bg_image_path):
     global music_on, current_theme_index, volume
@@ -24,8 +17,7 @@ def show_settings(screen, current_bg_image_path):
     slider_handle_rect = pygame.Rect(slider_rect.x + volume * slider_rect.width - 10, slider_rect.y - 10, 20, 30)  
     dragging = False 
     return_button_rect = pygame.Rect(550, 475, 200, 50)  
-    change_music_button_rect = pygame.Rect(550, 375, 200, 50) 
-    button_color = (255, 0, 0)  
+    change_music_button_rect = pygame.Rect(550, 375, 200, 50)
 
     running = True
     while running:
@@ -70,22 +62,21 @@ def show_settings(screen, current_bg_image_path):
                     elif mouse_x > slider_rect.x + slider_rect.width:
                         slider_handle_rect.x = slider_rect.x + slider_rect.width - 20
 
-
                     volume = (slider_handle_rect.x - slider_rect.x) / (slider_rect.width - 20)
                     volume = max(0.0, min(volume, 1.0))
                     pygame.mixer.music.set_volume(volume)
-        screen.blit(bg_image, (0, 0))
-        music_button_color = (255, 0, 0) if music_on else (0, 0, 255)
+        draw_image(screen, bg_image, 0, 0)
+        music_button_color = RED if music_on else BLUE
         music_button_rect = pygame.Rect(550, 200, 200, 50)
         pygame.draw.rect(screen, music_button_color, music_button_rect)
-        draw_text(screen, "Music: ON" if music_on else "Music: OFF", 24, (255, 255, 255), music_button_rect.centerx, music_button_rect.centery, center=True)
-        pygame.draw.rect(screen, (200, 200, 200), slider_rect)  
-        pygame.draw.rect(screen, (255, 0, 0), slider_handle_rect)  
-        draw_text(screen, f"Volume: {int(volume * 100)}%", 24, (255, 255, 255),slider_rect.centerx, slider_rect.y - 30, center=True)
-        pygame.draw.rect(screen, button_color, change_music_button_rect)
-        draw_text(screen, "Change Theme", 24, (255, 255, 255), change_music_button_rect.centerx, change_music_button_rect.centery, center=True)
-        pygame.draw.rect(screen, button_color, return_button_rect)
-        draw_text(screen, "Save & Return", 24, (255, 255, 255), return_button_rect.centerx, return_button_rect.centery, center=True)
+        draw_text(screen, "Music: ON" if music_on else "Music: OFF", 24, WHITE, music_button_rect.centerx, music_button_rect.centery, center=True)
+        pygame.draw.rect(screen, GRAY, slider_rect)
+        pygame.draw.rect(screen, RED, slider_handle_rect)
+        draw_text(screen, f"Volume: {int(volume * 100)}%", 24, WHITE,slider_rect.centerx, slider_rect.y - 30, center=True)
+        pygame.draw.rect(screen, RED, change_music_button_rect)
+        draw_text(screen, "Change Theme", 24, WHITE, change_music_button_rect.centerx, change_music_button_rect.centery, center=True)
+        pygame.draw.rect(screen, RED, return_button_rect)
+        draw_text(screen, "Save & Return", 24, WHITE, return_button_rect.centerx, return_button_rect.centery, center=True)
 
         pygame.display.flip()
 
