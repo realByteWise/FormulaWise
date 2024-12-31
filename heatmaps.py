41614c1cfa87e1ec3bf22bab101e5048eb1f8071
ByteWise <157060515+realByteWise@users.noreply.github.com>
2024-12-31 20:59:07 +0400

    Revamp Heatmaps Page

--- heatmaps.py
+++ heatmaps.py
@@ -2,16 +2,17 @@ import sys
 import matplotlib as mpl
 import numpy as np
 import fastf1.plotting
+import pygame_widgets
 from resources import *
 from matplotlib import pyplot as plt
 from matplotlib.collections import LineCollection
+from pygame_widgets.dropdown import Dropdown
 
 
-def plot_heatmap(driver, year, week):
-    global error_message
+def plot_heatmap(driver, year, gp):
     fastf1.plotting.setup_mpl(mpl_timedelta_support=False, misc_mpl_mods=False, color_scheme='fastf1')
     try:
-        session = ff1.get_session(year, week, 'R')
+        session = ff1.get_session(year, gp, 'R')
         session.load(weather=False, messages=False)
         lap = session.laps.pick_drivers(driver).pick_fastest()
         x = lap.telemetry['X']
@@ -33,92 +34,149 @@ def plot_heatmap(driver, year, week):
         plt.show()
     except Exception as e:
         print(f"Error: {e}")  # Debugging
-        error_message = "Error fetching data. Please try again."
-        return False
-    return True
+        return "Error fetching data. Please try again."
+    return "Please check the loaded window for the data."
 
 def heatmaps(screen,current_bg_image_path):
-    global error_message
     bg_image = pygame.image.load(current_bg_image_path).convert()
     bg_image = pygame.transform.scale(bg_image, (WIDTH, HEIGHT))
     logo_image = pygame.image.load(logo_image_path)
     logo_image = pygame.transform.scale(logo_image, (logo_width, logo_height))
     input_width, input_height = 300, 40
-    driver_box = pygame.Rect(WIDTH // 2 - input_width // 2, HEIGHT // 2 - 100, input_width, input_height)
-    year_box = pygame.Rect(WIDTH // 2 - input_width // 2, HEIGHT // 2 - 30, input_width, input_height)
-    week_box = pygame.Rect(WIDTH // 2 - input_width // 2, HEIGHT // 2 + 40, input_width, input_height)
-    submit_button = pygame.Rect(WIDTH // 2 - 75, HEIGHT // 2 + 100, 150, 50)
-    return_button = pygame.Rect(WIDTH // 2 - 75, HEIGHT // 2 + 160, 150, 50)
+    year_box = pygame.Rect(int(WIDTH * (3 / 4)) - input_width // 2, 30, input_width, input_height)
+    submit_button = pygame.Rect(885, 270, 150, 50)
+    return_button = pygame.Rect(245, 550, 150, 50)
+    return_prev_button = pygame.Rect(855, 340, 210, 50)
 
-    driver_text = ""
     year_text = ""
-    week_text = ""
     active_box = None
     error_message = ""
+    stage = 1
+    gp_dropdown = None
+    drivers_dropdown = None
     running = True
 
     while running:
-        for event in pygame.event.get():
+        events = pygame.event.get()
+        for event in events:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
 
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if driver_box.collidepoint(event.pos):
-                    active_box = "driver"
-                elif year_box.collidepoint(event.pos):
+                if year_box.collidepoint(event.pos) and stage == 1:
                     active_box = "year"
-                elif week_box.collidepoint(event.pos):
-                    active_box = "week"
                 elif submit_button.collidepoint(event.pos):
-                    if driver_text and year_text and week_text:
-                        if plot_heatmap(driver_text, int(year_text), int(week_text)):
-                            driver_text, year_text, week_text = "", "", ""
-                        else:
-                            error_message = "Invalid input. Please try again."
+                    if year_text:
+                        if stage == 1:
+                            try:
+                                session = ff1.get_event_schedule(int(year_text))
+                                countries = session["EventName"].tolist()
+
+                                if countries[0] == "Pre-Season Testing":
+                                    countries.remove("Pre-Season Testing")
+                                error_message = ""
+
+                                gp_dropdown = Dropdown(
+                                    screen, 685, 90, 550, 25, name='Select Grand Prix',
+                                    choices=countries, fontSize=25, borderRadius=5, textHAlign='centre',
+                                    colour=pygame.Color('gray'), values=countries, direction='down'
+                                )
+                                stage = 2
+                            except Exception:
+                                error_message = "Please enter a valid year."
+
+                        elif stage == 2 and gp_dropdown:
+                            if gp_dropdown.getSelected() is not None:
+                                try:
+                                    session = ff1.get_session(int(year_text), gp_dropdown.getSelected(), 'R')
+                                    session.load(laps=False, weather=False, messages=False)
+                                    drivers = [driver for driver in session.results['Abbreviation']]
+
+                                    gp_dropdown.toggleDropped() if gp_dropdown.isDropped() else ...
+                                    gp_dropdown.hide()
+                                    stage = 3
+                                    error_message = ""
+
+                                    drivers_dropdown = Dropdown(
+                                        screen, 850, 20, 220, 27, name='Select Driver',
+                                        choices=drivers, fontSize=25, borderRadius=5, textHAlign='centre',
+                                        colour=pygame.Color('gray'), values=drivers, direction='down'
+                                    )
+                                except Exception:
+                                    error_message = "Error fetching data. Please try again."
+                            else:
+                                error_message = "Please select a Grand Prix."
+
+                        elif stage == 3 and drivers_dropdown:
+                            if drivers_dropdown.getSelected() is not None:
+                                error_message =  plot_heatmap(drivers_dropdown.getSelected(), int(year_text), gp_dropdown.getSelected())
+                            else:
+                                error_message = "Please select a Driver."
                     else:
-                        error_message = "Please fill all fields."
+                        error_message = "Please enter year."
+                elif return_prev_button.collidepoint(event.pos):
+                    if drivers_dropdown:
+                        drivers_dropdown.toggleDropped() if drivers_dropdown.isDropped() else ...
+                        drivers_dropdown.hide()
+                    stage = 1
+                    error_message = ""
                 elif return_button.collidepoint(event.pos):
-                    return  
+                    if gp_dropdown:
+                        gp_dropdown.toggleDropped() if gp_dropdown.isDropped() else ...
+                        gp_dropdown.hide()
+                    if drivers_dropdown:
+                        drivers_dropdown.toggleDropped() if drivers_dropdown.isDropped() else ...
+                        drivers_dropdown.hide()
+                    return
 
             if event.type == pygame.KEYDOWN:
-                if active_box == "driver":
-                    if event.key == pygame.K_BACKSPACE:
-                        driver_text = driver_text[:-1]
-                    else:
-                        driver_text += event.unicode
-                elif active_box == "year":
+                if active_box == "year":
                     if event.key == pygame.K_BACKSPACE:
                         year_text = year_text[:-1]
                     else:
                         year_text += event.unicode
-                elif active_box == "week":
-                    if event.key == pygame.K_BACKSPACE:
-                        week_text = week_text[:-1]
-                    else:
-                        week_text += event.unicode
 
         draw_image(screen, bg_image, 0, 0)
-        draw_image(screen, logo_image, WIDTH // 2, logo_image.get_height() - 30, center=True)
-        draw_text(screen, "Heatmaps", 80, WHITE, WIDTH // 2, logo_image.get_height() + 40, center=True)
+        draw_image(screen, logo_image, WIDTH // 4, logo_image.get_height() - 30, center=True)
+        draw_text(screen, "Heatmaps", 80, WHITE, WIDTH // 4, logo_image.get_height() + 40, center=True)
 
-        pygame.draw.rect(screen, LIGHT_GRAY, driver_box)
-        pygame.draw.rect(screen, LIGHT_GRAY, year_box)
-        pygame.draw.rect(screen, LIGHT_GRAY, week_box)
-        pygame.draw.rect(screen, RED, submit_button)
+        description_lines = [
+            "This feature allows you to select a previous Grand Prix of the year entered.",
+            "You can view the standings of the race along with the podium finishers.",
+            "Simply enter the desired year and click submit to see the available races.",
+            "Select a Grand Prix from the dropdown menu to view detailed results.",
+            "The standings will include driver positions, points earned, and team information.",
+            "Explore historical data to analyze past performances and trends.",
+            "This feature is perfect for fans wanting to revisit memorable races.",
+            "Stay updated with the latest statistics and records from previous seasons.",
+            "Easily navigate through different years to find your favorite races.",
+            "Feature implementation date: 19/12/2024."
+        ]
+        description_start_y = logo_image.get_height() + 100
+
+        for i, line in enumerate(description_lines):
+            draw_text(screen, line, 24, WHITE, 45, description_start_y + i * 30, center=False)
+
         pygame.draw.rect(screen, RED, return_button)
+        draw_text(screen, "Return to Menu", 24, WHITE, return_button.centerx, return_button.centery, center=True)
+        pygame.draw.rect(screen, RED, submit_button)
 
-        draw_text(screen,"Driver", 24, WHITE, driver_box.centerx, driver_box.centery - 40, center=True)
-        draw_text(screen,"Year", 24, WHITE, year_box.centerx, year_box.centery - 35, center=True)
-        draw_text(screen,"Week", 24, WHITE, week_box.centerx, week_box.centery - 35, center=True)
-        draw_text(screen,"Submit", 24, WHITE, submit_button.centerx, submit_button.centery, center=True)
-        draw_text(screen,"Return to Menu", 24, WHITE, return_button.centerx, return_button.centery, center=True)
-
-        draw_text(screen,driver_text, 24, BLACK, driver_box.centerx, driver_box.centery, center=True)
-        draw_text(screen,year_text, 24, BLACK, year_box.centerx, year_box.centery, center=True)
-        draw_text(screen,week_text, 24, BLACK, week_box.centerx, week_box.centery, center=True)
+        if stage == 3:
+            draw_text(screen, "Submit", 24, WHITE, submit_button.centerx, submit_button.centery, center=True)
+            pygame.draw.rect(screen, RED, return_prev_button)
+            draw_text(screen, "Return to Previous Menu", 24, WHITE, return_prev_button.centerx, return_prev_button.centery, center=True)
+        else:
+            pygame.draw.rect(screen, LIGHT_GRAY, year_box)
+            draw_text(screen, "Year", 24, WHITE, year_box.centerx, year_box.centery - 30, center=True)
+            draw_text(screen, year_text, 24, BLACK, year_box.centerx, year_box.centery, center=True)
+            if stage == 1:
+                draw_text(screen, "Load", 24, WHITE, submit_button.centerx, submit_button.centery, center=True)
+            elif stage == 2:
+                draw_text(screen, "Submit", 24, WHITE, submit_button.centerx, submit_button.centery, center=True)
 
         if error_message:
-            draw_text(screen,error_message, 20, RED, WIDTH // 2, HEIGHT // 2 + 200, center=True)
+            draw_text(screen,error_message, 30, RED, int(WIDTH * (3 / 4)), 240, center=True)
 
+        pygame_widgets.update(events)
         pygame.display.flip()
